3a8155dd8b2062bbd4d857c9a899c4f8640eb76b
SonicBSV <sonicbsv@gmail.com>
2022-08-15 07:08:41 +0300

    add vendor_framework_compatibility_matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,6 +87,7 @@ LOC_HIDL_VERSION := 4.0
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += NFC
 ODM_MANIFEST_NFC_FILES := $(COMMON_PATH)/manifest_nfc.xml
