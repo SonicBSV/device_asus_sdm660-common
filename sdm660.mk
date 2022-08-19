75af4c3078735e3dbe5bf7cf27d9e98fe409bf54
SonicBSV <sonicbsv@gmail.com>
2022-08-19 09:39:12 +0300

    revert Device uses high-density artwork

--- sdm660.mk
+++ sdm660.mk
@@ -14,11 +14,6 @@ $(call inherit-product, vendor/asus/sdm660-common/sdm660-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COMPRESSED_APEX := false
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-PRODUCT_AAPT_PREBUILT_DPI := xxhdpi
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
